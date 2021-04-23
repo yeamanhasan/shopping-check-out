93319329c1499f3cbc9effef8abb4597674b0286
yeamanhasan <yeamanhasan1997@gmail.com>
2021-04-23 22:01:47 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// document.getElementById('case-increase').addEventListener('click', function(){
+//     const caseInput = document.getElementById('case-count');
+//     const caseCount = parseInt(caseInput.value);
+//     const caseNewCount = caseCount + 1;
+//     caseInput.value = caseNewCount;
+// })
+
+document.getElementById('case-increase').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-count');
+    const caseCount = parseInt(caseInput.value);
+    const caseNewCount = caseCount + 1;
+    caseInput.value = caseNewCount;
+})
+document.getElementById('case-increase').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-count');
+    const caseCount = parseInt(caseInput.value);
+    const caseNewCount = caseCount + 1;
+    caseInput.value = caseNewCount;
+})
